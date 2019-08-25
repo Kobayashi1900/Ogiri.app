7735dccd590a18ba16dc6e787bf219a6a1bc2b73
KobayashiRiku <cccc12233@yahoo.co.jp>
2019-08-26 03:08:40 +0900

    Initial Commit

---
+++ OgiriTests/OgiriTests.swift
@@ -0,0 +1,34 @@
+//
+//  OgiriTests.swift
+//  OgiriTests
+//
+//  Created by kobayashi riku on 2019/08/26.
+//  Copyright © 2019 kobayashi riku. All rights reserved.
+//
+
+import XCTest
+@testable import Ogiri
+
+class OgiriTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
