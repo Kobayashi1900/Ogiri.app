a6897d030058c6fe01ab2279a67467f1e9549c31
阿部優太 <yutaabe200@gmail.com>
2019-11-08 16:12:55 +0900

    APIClient周り修正

--- Ogiri/Controller/PlayViewController.swift
+++ Ogiri/Controller/PlayViewController.swift
@@ -14,29 +14,80 @@ import Photos
 
 class PlayViewController: UIViewController {
     
-    
     @IBOutlet weak var odaiImageView: UIImageView!
     @IBOutlet weak var commentTextView: UITextView!
     
-    let wordsList = WordsList()  //インスタンス生成、語群にアクセスできる
-    var count = 0
+    private let wordsList = WordsList()  //インスタンス生成、語群にアクセスできる
+    private var count = 0
     
+    private let baseUrl = "https://pixabay.com/api/"
+    private let apiKey = "13787747-8afd4e03ae250892260a92055"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        getImages()
-
+        //getImages()
+        self.getPixabayImages()
     }
     
+    // パラメータ作成
+    private func getPixabayImages() {
+        let parameters: [String: Any] = [
+            "key": self.apiKey,
+            "q": "\(wordsList.wordsA.randomElement()!)+\(wordsList.wordsB.randomElement()!)",
+            "per_page": 200
+        ]
+        let headers: HTTPHeaders = [
+            "Contenttype": "application/json"
+        ]
+        
+        self.fire(params: parameters, headers: headers)
+    }
+    
+    // APIをコールする
+    private func fire(params: [String: Any], headers: [String: String]) {
+        Alamofire.request(self.baseUrl,
+                          method: .get,
+                          parameters: params,
+                          encoding: URLEncoding.queryString,
+                          headers: headers)
+            .responseData { (response) in
+                self.result(response: response)
+        }
+    }
+    
+    private func result(response: DataResponse<Data>) {
+        let json :JSON = JSON(response.data)
+        let totalHitsCount = json ["hits"].array?.count
+        
+        // 0件だった場合さ再度APIコールし処理を終了(return)
+        if totalHitsCount == 0 {
+            self.getPixabayImages()
+            return
+        }
+        switch response.result {
+        case .success:
+            guard let data = response.data else { fatalError() }
+            self.displayImage(data: data)
+            break
+            
+        case .failure(let error):
+            print(error.localizedDescription)
+            break
+        }
+    }
+    
+    private func displayImage(data: Data) {
+        let json :JSON = JSON(data as Any)
+        let totalHitsCount = json ["hits"].array?.count
+        
+        let totalHitsRandomNumber = Int.random(in: 0..<totalHitsCount!)
+        guard let imageString = json ["hits"][totalHitsRandomNumber]["webformatURL"].string else { return }
+        
+        self.odaiImageView.sd_setImage(with: URL(string: imageString), completed: { (image, err, cacheType, url) in
+            print(err?.localizedDescription as Any)
+        })
+    }
 
-    
-    
-    
-    
-    
-    
-    
     //画像を引っ張ってくる(pixabay.com)
     func getImages() {
         
