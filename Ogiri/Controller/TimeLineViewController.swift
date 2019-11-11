241e4a9edd9a971ed6cc0a3f04846fa1ce194fc0
KobayashiRiku <cccc12233@yahoo.co.jp>
2019-11-11 17:37:13 +0900

    TableViewCell関係のエラー

--- Ogiri/Controller/TimeLineViewController.swift
+++ Ogiri/Controller/TimeLineViewController.swift
@@ -11,19 +11,24 @@ import Firebase
 import FirebaseAuth
 
 class TimeLineViewController:
-        UIViewController{
-//      UITableViewDelegate,
-//      UITableViewDataSource {
+      UIViewController,
+      UITableViewDelegate,
+      UITableViewDataSource {
 
 
     
     @IBOutlet weak var timeLineTableView: UITableView!
-
+    
+    
+    
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        timeLineTableView.delegate = self
-//        timeLineTableView.dataSource = self
+        timeLineTableView.delegate = self
+        timeLineTableView.dataSource = self  //デリゲートメソッドが使えるようになる
 
     }
     
@@ -39,19 +44,29 @@ class TimeLineViewController:
     }
 
 
-//    //セクションの中のセルの数(必須)
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//
-//
-//
-//    }
-//
-//    //セルをどうやって構築するか(必須)
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//
-//
-//    }
+    //セクションの中のセルの数(必須)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return 1
+
+    }
+
+    //セルをどうやって構築するか(必須)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        //.dequeueReusableCell - 識別子がついたセルのサイズを変更する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+        
+    }
+    
+    //セルの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return view.frame.size.height/2
+        
+    }
 
 
 
