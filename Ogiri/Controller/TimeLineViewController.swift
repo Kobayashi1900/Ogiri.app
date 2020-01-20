08e403fddbb9e90cd0e694bf67057a0448e3e26b
小林 <cccc12233@yahoo.co.jp>
2020-01-20 23:39:50 +0900

    全てのユーザーのcomment/userName/postedAt の表示に成功/画像の取得のコード移動

--- Ogiri/Controller/TimeLineViewController.swift
+++ Ogiri/Controller/TimeLineViewController.swift
@@ -23,13 +23,12 @@ class TimeLineViewController:
     var XodaiImage2:URL? = nil  //
     var XodaiImage3:URL? = nil  //
     var XodaiImage4:URL? = nil  //
-    var Xcomment1:String = ""  //firebaseからDLしたコメントを代入する変数
-    var Xcomment2:String = ""  //
-    var Xcomment3:String = ""  //
-    var Xcomment4:String = ""  //
+//    var Xcomment1:String = ""  //firebaseからDLしたコメントを代入する変数
+//    var Xcomment2:String = ""  //
+//    var Xcomment3:String = ""  //
+//    var Xcomment4:String = ""  //
     var storagerefProfileImage:StorageReference? = nil  //storageからDLしたプロフィール画像を取得するための変数
     
-    var kaitouArray: [Kaitou?] = [nil, nil, nil, nil]
     var kaitouArray2: [Kaitou?] = []  //ユーザーの4題の回答のデータをまとめ、のちにkaitouArrayに入れる
     
    
@@ -51,18 +50,6 @@ class TimeLineViewController:
 //                print("登録ユーザー")
 //            }
 
-            //profileImageの取得
-            storagerefProfileImage = Storage.storage().reference(forURL: "gs://ogiri-d1811.appspot.com").child("profileImage")
-            
-            storagerefProfileImage?.listAll(completion: { (StorageListResult, error) in
-                if let error = error {
-                    print(".listAllのエラー:\(error)")
-                } else {
-                    for ref in StorageListResult.items {
-                        print("プロフ画:\(ref)")
-                    }
-                }
-            })//全ユーザーのprofileImageは取得できてる
 
 
             //userNameとpostedAtとuidとcomment1~4を取得し、配列kaitouArray2に格納
@@ -99,8 +86,58 @@ class TimeLineViewController:
                     self.timeLineTableView.reloadData()
                 }
             }
+      }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        }
+    
+    
+
+    //セクションの数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
 
+    //セクションの中のセルの数(必須)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return kaitouArray2.count
+    }
+
+    //セルをどうやって構築するか(必須)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        //識別子がついたセルのサイズを変更する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
+        
+        //配列が空の時
+        if kaitouArray2[indexPath.row] == nil {
+            // データが存在していないので、セルに何もせずそのまま返却して終了。
+            return cell
+        }
+        
+        //Outlet接続できないため、タグでコンテンツを管理する(このメソッド内でのみ有効)
+        let profileImageView = cell.viewWithTag(1) as! UIImageView
+        let userNameLabel = cell.viewWithTag(2) as! UILabel
+        let postedAtLabel = cell.viewWithTag(3) as! UILabel
+        let odaiImageView = cell.viewWithTag(4) as! UIImageView
+        let commentTextView = cell.viewWithTag(5) as! UITextView
+        
+        //profileImageの取得
+        storagerefProfileImage = Storage.storage().reference(forURL: "gs://ogiri-d1811.appspot.com").child("profileImage")
+        
+        storagerefProfileImage?.listAll(completion: { (StorageListResult, error) in
+            if let error = error {
+                print(".listAllのエラー:\(error)")
+            } else {
+                for ref in StorageListResult.items {
+                    print("プロフ画:\(ref)")
+                }
+            }
+        })//全ユーザーのprofileImageは取得できてる
+        
         ////↓odaiImageNumber1~4取得↓////
         let storageRefOdaiImage1 = Storage.storage().reference(forURL: "gs://ogiri-d1811.appspot.com").child("odaiImageNumber1")
             storageRefOdaiImage1.listAll(completion: { (StorageListResult, error) in
@@ -153,51 +190,12 @@ class TimeLineViewController:
                         }
                     }
             })//全ユーザーの4題目の画像(odaiImageNumber1)は取得できてる
-      }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        }
-    
-    
-
-    //セクションの数
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-
-    //セクションの中のセルの数(必須)
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return kaitouArray.count
-    }
-
-    //セルをどうやって構築するか(必須)
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        //識別子がついたセルのサイズを変更する
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TableViewCell
         
-        //配列が空の時
-        if kaitouArray2[indexPath.row] == nil {
-            // データが存在していないので、セルに何もせずそのまま返却して終了。
-            return cell
-        }
         
-        //Outlet接続できないため、タグでコンテンツを管理する(このメソッド内でのみ有効)
-        let profileImageView = cell.viewWithTag(1) as! UIImageView
-        let userNameLabel = cell.viewWithTag(2) as! UILabel
-        let postedAtLabel = cell.viewWithTag(3) as! UILabel
-        let odaiImageView = cell.viewWithTag(4) as! UIImageView
-        let commentTextView = cell.viewWithTag(5) as! UITextView
             
+        
         //odaiImageViewへの表示
-//        odaiImageView.sd_setImage(with: kaitouArray2[indexPath.row]?.odaiImage, completed: {_, _, _, imageUrl in
-//
-//            print("odaiImage:\(self.kaitouArray[indexPath.row]?.odaiImage)")
-//                                print("imageUrl:\(imageUrl)")
-//                            })
+//        odaiImageView.sd_setImage(with: )
         
         //commentTextViewへの表示
         commentTextView.text = kaitouArray2[indexPath.row]?.comment
